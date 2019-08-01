480171fd0e9078810e00735a355ea583cbf0df7b
Siuungsal <unggar1pengincar@gmail.com>
2019-08-01 10:20:54 +0700

    Toolbar dan Floating Button
    Toolbar dan Floating Button

--- BBT/app/src/main/java/com/example/bbt/adminlist.java
+++ BBT/app/src/main/java/com/example/bbt/adminlist.java
@@ -1,17 +1,23 @@
 package com.example.bbt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class adminlist extends AppCompatActivity implements View.OnClickListener{
-    private Button btnbudidaya, btnpenghas, btnproteksi, btnchat, btnout;
+    private Button btnbudidaya, btnpenghas, btnproteksi;//, btnchat, btnout;
     private String name;
     private FirebaseAuth firebaseAuth;
 
@@ -20,6 +26,9 @@ public class adminlist extends AppCompatActivity implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminlist);
 
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
         firebaseAuth = FirebaseAuth.getInstance();
 
         name = getIntent().getExtras().get("user_name").toString();
@@ -27,19 +36,21 @@ public class adminlist extends AppCompatActivity implements View.OnClickListener
         btnbudidaya = findViewById(R.id.btnbudidaya);
         btnpenghas = findViewById(R.id.btnpenghas);
         btnproteksi = findViewById(R.id.btnproteksi);
-        btnchat = findViewById(R.id.btnchat);
-        btnout = findViewById(R.id.btnout);
+        //btnchat = findViewById(R.id.btnchat);
+        //btnout = findViewById(R.id.btnout);
+        FloatingActionButton fbtnchat = findViewById(R.id.fbtnchat1);
 
         btnbudidaya.setOnClickListener(this);
         btnpenghas.setOnClickListener(this);
         btnproteksi.setOnClickListener(this);
-        btnchat.setOnClickListener(this);
-        btnout.setOnClickListener(this);
+        //btnchat.setOnClickListener(this);
+        //btnout.setOnClickListener(this);
+        fbtnchat.setOnClickListener(this);
     }
 
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btnchat:
+            /*case R.id.btnchat:
                 login();
                 break;
             case R.id.btnout:
@@ -47,6 +58,9 @@ public class adminlist extends AppCompatActivity implements View.OnClickListener
                 Toast.makeText(getApplicationContext(), "Signing Out", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(this, MainActivity.class));
                 finish();
+                break;*/
+            case R.id.fbtnchat1:
+                login();
                 break;
         }
     }
@@ -55,6 +69,27 @@ public class adminlist extends AppCompatActivity implements View.OnClickListener
         Intent intent = new Intent(adminlist.this, Main3Activity.class);
         intent.putExtra("user_name", name);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.more_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.signout:
+                firebaseAuth.signOut();
+                Toast.makeText(getApplicationContext(), "Signing Out", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
--- BBT/app/src/main/java/com/example/bbt/userlist.java
+++ BBT/app/src/main/java/com/example/bbt/userlist.java
@@ -2,17 +2,23 @@ package com.example.bbt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class userlist extends AppCompatActivity implements View.OnClickListener{
 
-    private Button btnbudidaya, btnpenghas, btnproteksi, btnchat, btnout;
+    private Button btnbudidaya, btnpenghas, btnproteksi;//, btnchat, btnout;
     private String name;
     private FirebaseAuth firebaseAuth;
 
@@ -21,6 +27,9 @@ public class userlist extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlist);
 
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar);
+
         firebaseAuth = FirebaseAuth.getInstance();
 
         name = getIntent().getExtras().get("user_name").toString();
@@ -28,20 +37,22 @@ public class userlist extends AppCompatActivity implements View.OnClickListener{
         btnbudidaya = findViewById(R.id.btnbudidaya);
         btnpenghas = findViewById(R.id.btnpenghas);
         btnproteksi = findViewById(R.id.btnproteksi);
-        btnchat = findViewById(R.id.btnchat);
-        btnout = findViewById(R.id.btnout);
+        //btnchat = findViewById(R.id.btnchat);
+        //btnout = findViewById(R.id.btnout);
+        FloatingActionButton fbtnchat = findViewById(R.id.fbtnchat);
 
         btnbudidaya.setOnClickListener(this);
         btnpenghas.setOnClickListener(this);
         btnproteksi.setOnClickListener(this);
-        btnchat.setOnClickListener(this);
-        btnout.setOnClickListener(this);
+        //btnchat.setOnClickListener(this);
+        //btnout.setOnClickListener(this);
+        fbtnchat.setOnClickListener(this);
 
     }
 
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btnchat:
+            /*case R.id.btnchat:
                 login();
                 break;
             case R.id.btnout:
@@ -49,6 +60,9 @@ public class userlist extends AppCompatActivity implements View.OnClickListener{
                 Toast.makeText(getApplicationContext(), "Signing Out", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(this, MainActivity.class));
                 finish();
+                break;*/
+            case R.id.fbtnchat:
+                login();
                 break;
         }
     }
@@ -59,4 +73,24 @@ public class userlist extends AppCompatActivity implements View.OnClickListener{
         startActivity(intent);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.more_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.signout:
+                firebaseAuth.signOut();
+                Toast.makeText(getApplicationContext(), "Signing Out", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
