062d1a3da5134149a237abe97255bab281bc3b2a
Siuungsal <unggar1pengincar@gmail.com>
2019-09-12 13:24:45 +0700

    tentang kami, bantuan, contact us, dan modifikasi chatroom
    modifikasi agar chatroom menampilkan bubble yang lebih konsisten jaraknya dan ukurannya. pada kelas MessageAdapter dalam method OnBindViewHolder, receiver dan sendermessagetext di set dengan text dummy agar ada titik defaultnya. default nya juga wrap content. Ketika jumlah karakter yang diketikkan melebihi 37 karakter maka akan di set menjadi match constraint. Sehingga bubble tetap utuh ukurannya.

--- BBT/app/src/main/java/com/example/bbt/MessageAdapter.java
+++ BBT/app/src/main/java/com/example/bbt/MessageAdapter.java
@@ -1,14 +1,17 @@
 package com.example.bbt;
 
+import android.media.Image;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -34,6 +37,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     public class MessageViewHolder extends RecyclerView.ViewHolder{
 
         public TextView senderMessageText, receiverMessageText, messageProfileName;
+        public ImageView checklist;
 
         public MessageViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -41,6 +45,8 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
             senderMessageText = (TextView) itemView.findViewById(R.id.sender_message_text);
             receiverMessageText = (TextView) itemView.findViewById(R.id.receiver_message_text);
             messageProfileName = (TextView) itemView.findViewById(R.id.message_profile_name);
+            checklist = (ImageView) itemView.findViewById(R.id.checklist);
+
         }
     }
 
@@ -69,21 +75,39 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
 
         holder.receiverMessageText.setVisibility(View.INVISIBLE);
         holder.messageProfileName.setVisibility(View.INVISIBLE);
+        holder.checklist.setVisibility(View.INVISIBLE);
+
+        holder.receiverMessageText.setText("dummy");
+        holder.senderMessageText.setText("dummy");
+
+        holder.receiverMessageText.getLayoutParams().width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        holder.receiverMessageText.requestLayout();
+
+        holder.senderMessageText.getLayoutParams().width = ViewGroup.LayoutParams.WRAP_CONTENT;
+        holder.senderMessageText.requestLayout();
 
         if(fromUserID.equals(messageSenderID)){
 
+            int banyakhuruf = 0;
+
             /*if(fromMod.equals("true")){
-                holder.senderMessageText.setBackgroundResource(R.drawable.messages_layout_admin);
-            }
-            else{
-                holder.senderMessageText.setBackgroundResource(R.drawable.sender_messages_layout);
+                //holder.senderMessageText.setBackgroundResource(R.drawable.messages_layout_admin);
             }*/
 
             holder.senderMessageText.setVisibility(View.VISIBLE);
-            holder.senderMessageText.setBackgroundResource(R.drawable.sender_messages_layout);
+
+            //holder.senderMessageText.setBackgroundResource(R.drawable.text_bubble_sender_smallest2);
             holder.senderMessageText.setText(messages.getMessage());
+
+            banyakhuruf = holder.senderMessageText.getText().toString().length();
+
+            if(banyakhuruf>37){
+                holder.senderMessageText.getLayoutParams().width = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT;
+                holder.senderMessageText.requestLayout();
+            }
         }
         else{
+            int banyakhuruf = 0;
             holder.senderMessageText.setVisibility(View.INVISIBLE);
             holder.messageProfileName.setVisibility(View.VISIBLE);
             holder.receiverMessageText.setVisibility(View.VISIBLE);
@@ -91,15 +115,26 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
             if(fromMod.equals("true")){
                 //holder.receiverMessageText.setBackgroundResource(R.drawable.messages_layout_admin);
                 //holder.messageProfileName.setBackgroundResource(R.drawable.messages_layout_admin);
+                holder.checklist.setVisibility(View.VISIBLE);
             }
-            else{
+            /*else{
                 //holder.receiverMessageText.setBackgroundResource(R.drawable.receiver_messages_layout);
                 //holder.messageProfileName.setBackgroundResource(R.drawable.messages_layout_user);
-            }
+            }*/
 
-            holder.receiverMessageText.setBackgroundResource(R.drawable.receiver_messages_layout);
+            //holder.receiverMessageText.setBackgroundResource(R.drawable.text_bubble_receiver_smallest2);
             holder.receiverMessageText.setText(messages.getMessage());
             holder.messageProfileName.setText(messages.getNama());
+
+            /*if(holder.receiverMessageText.toString().length()>37){
+                holder.receiverMessageText.setWidth(0);
+            }*/
+            banyakhuruf = holder.receiverMessageText.getText().toString().length();
+
+            if(banyakhuruf>37){
+                holder.receiverMessageText.getLayoutParams().width = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT;
+                holder.receiverMessageText.requestLayout();
+            }
         }
     }
 
